377f2683c41908d3e22fb9cad583b727b9a498ca
Ayoub B <ayoub.biferdou@e-polytechnique.ma>
2024-07-31 14:49:17 +0100

    Init OpenGL 4.6 project

---
+++ Main.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+
+int main()
+{
+	// Initialize GLFW
+	glfwInit();
+
+	// Set GLFW window hints
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Create GLFW window
+	GLFWwindow* window = glfwCreateWindow(800, 800, "OpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	// Make the window's context current
+	glfwMakeContextCurrent(window);
+
+	// Load OpenGL functions
+	gladLoadGL();
+
+	// Set the viewport
+	glViewport(0, 0, 800, 800);
+
+	// Main loop
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	// Clean up
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
